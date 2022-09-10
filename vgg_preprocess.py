98d712d56d660634a8f0cb362409d81a8b990f23
MyrtooS <63005163+MyrtooS@users.noreply.github.com>
2022-09-10 21:30:14 +0300

    Update vgg_preprocess.py

--- vgg_preprocess.py
+++ vgg_preprocess.py
@@ -2,13 +2,16 @@ import cv2
 import tensorflow as tf
 import numpy as np
 
+#image preprocessing for VGG16 model
 
+#preprocessing for Integrated Gradients
 def preprocess_ig(image):
   image = cv2.resize(image, (224, 224))
   image = tf.keras.preprocessing.image.img_to_array(image)
   image = tf.convert_to_tensor(image, dtype=tf.float32)
   return image
 
+#preprocessing for Saliency Maps
 def preprocess_saliency(image):
   image = cv2.resize(image, (224, 224))
   image = np.expand_dims(image, axis=0)
@@ -16,6 +19,7 @@ def preprocess_saliency(image):
   image = image.reshape(1,224,224,3)
   return image
 
+#preprocessing for Grad-CAM
 def gradcam_preprocess(image):
   image = cv2.resize(image, (224, 224))
   image = np.expand_dims(image, axis=0)
