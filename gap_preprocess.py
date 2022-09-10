ea8de0e569962969d4f413db72453f25d4d80655
MyrtooS <63005163+MyrtooS@users.noreply.github.com>
2022-09-10 21:27:30 +0300

    Update gap_preprocess.py

--- gap_preprocess.py
+++ gap_preprocess.py
@@ -3,12 +3,15 @@ import tensorflow as tf
 import numpy as np
 
 #this is the pre processing for images for the 1st and 2nd CNN models (CNN and CNN with 1x1 Convolutions)
+
+#image preprocess for Integrated Gradients
 def preprocess_ig(image):
   image = cv2.resize(image, (224, 224))
   image = tf.keras.preprocessing.image.img_to_array(image)
   image = tf.convert_to_tensor(image, dtype=tf.float32)
   return image
 
+#image preprocess for Saliency Maps
 def preprocess_saliency(image):
     image = cv2.resize(image, (224, 224))
     image = image.astype('float32')/255.
@@ -16,6 +19,7 @@ def preprocess_saliency(image):
     image = image.reshape(1,224,224,3)
     return image
 
+#image preprocess for Grad-Cam
 def gradcam_preprocess(image):
   image = cv2.resize(image, (224, 224))
   image = image.astype('float32')/ 255.
