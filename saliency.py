e0c5cc02a5420d5295a1474b35a8482276e64de2
MyrtooS <63005163+MyrtooS@users.noreply.github.com>
2022-09-10 21:33:27 +0300

    Update saliency.py

--- saliency.py
+++ saliency.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def saliency_preproccess(image,model):
   images = tf.Variable(image, dtype=float)
 
-
+#calculate the gradient with respect to the top class score
   with tf.GradientTape() as tape:
       pred = model(images, training=False)
       class_idxs_sorted = np.argsort(pred.numpy().flatten())[::-1]
@@ -17,7 +17,7 @@ def saliency_preproccess(image,model):
 
 def make_saliency(grads):
   dgrad_abs = tf.math.abs(grads)
-
+#find the max of the absolute values of the gradient along each RGB channel
   dgrad_max_ = np.max(dgrad_abs, axis=3)[0]
 
 ## normalize to range between 0 and 1
